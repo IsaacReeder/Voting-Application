ea5975c1ae9fd645f3c43c80cacd8fa7975c4d48
Isaac R <isaacreeder5@gmail.com>
2021-01-15 19:48:25 +0100

    heroku commit

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ const MONGODB_URI =
 require("./server/routes/poll.routes.js")(app);
 
 mongoose
-  .connect(MONGODB_URI || "mongodb://localhost/voting", {
+  .connect(MONGODB_URI || "mongodb://localhost/votingDojo", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    const port = 9000;
+    const port = process.env.PORT || 9000;
     app.listen(port, () => {
       console.log(
         "Your server is listening on port %d (http://localhost:%d)",
@@ -30,6 +30,10 @@ mongoose
     console.log("Something went wrong with the database connection", error);
   });
 
+///Heroku
+
+const favicon = require("express-favicon");
+const path = require("path");
 app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
@@ -40,4 +44,3 @@ app.get("/ping", function (req, res) {
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(port);
