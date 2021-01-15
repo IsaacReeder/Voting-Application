c1f1f1193cdaaed63ca925542ab99eff5ef8daba
Isaac R <isaacreeder5@gmail.com>
2021-01-15 17:56:07 +0100

    server.js edit

--- server.js
+++ server.js
@@ -29,3 +29,17 @@ mongoose
   .catch((error) => {
     console.log("Something went wrong with the database connection", error);
   });
+
+//HEROKU DEPLOYMENT
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+//HEROKU DEPLOYEMENT
